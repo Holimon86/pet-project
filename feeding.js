270b17293c6667c8e8e32dd76b8c4a68f3bec3ba
Douglas <mercurymuur86@yahoo.com>
2023-02-23 11:56:33 -0500

    corso

--- feeding.js
+++ feeding.js
@@ -32,9 +32,9 @@ var feeding =[
 ]
 var postHTML = " "
 for (var i=0; i < feeding.length; i++){
-    var heading = "<div class='feeding col-lg-4'><span><h3>" + feeding[i].title + "</h3>"
+    var heading = "<div class='feeding col-lg-4'><span><h2>" + feeding[i].title + "</h2>"
     var image = '<img src="' + feeding[i].image + ' " style="width: 18rem; height: 250px;"/>'
-    var price = "<p> $" + feeding[i].price + "</p></span>"
+    var price = "<h3 ><span style='color:green;'<h2> $"+ feeding[i].price + "</h3></span>"
     var description = '<div class="success mb-5"><button>Add to Cart</button></div></div>'
     var concatThis = heading + image + price + description
     postHTML = postHTML + concatThis
--- pet.js
+++ pet.js
@@ -1,11 +1,46 @@
 var feeding = [
     {
         title: "One Week Raw Food Prep",
-        image: "./images/RawFoodDietLarg.webp"
+        image: "./images/RawFoodDietLarg.webp",
         price:  420.00,
     },
     {
-        title: "One Month Raw Food Prep",
-        image: "./images/rawFoodPrepTin.jpeg"
+        title:"One Month Raw Food Prep",
+        image: "./images/barfFoodMeal",
+        price: 1500.00,
+    },
+    {
+        title:"Raw Bagged Dog Food (Frozen).",
+        image:"./images/rawBagDogFood.jpeg",
+        price: 300.00,
+    },
+    
+    {
+        title: "Bag Puppy Food",
+        image: "./images/largePuppyDogFood.jpeg",
+        price:  300.00,
+    },
+    {
+        title:"Bag Adult Food",
+        image: "./images/largeDogFoodProcessed.webp",
+        price: 300.00,
+    },
+    {
+        title:"Large Dog Treats.",
+        image:"./images/largeDogTreatsForAgressiveChewers.jpg",
+        price: 300.00,
     }
+
 ]
+
+var postHTMl= " "
+for (var i=0; i < feeding.length; i++){
+    var heading = "<div class='feeding col-lg-4'><span><h3>" + feeding[i].title + "</h3>"
+    var image = '<img src="' + feeding[i].image + ' " style="width: 18rem; height: 250px;"/>'
+    var price = "<p> $" + feeding[i].price + "</p><button>Add to Cart</button></div></div>'<span>"
+    var concatThis = heading + image + price + description
+    postHTML = postHTML + concatThis
+}
+document.getElementById("feeding").innerHTML = postHTML
+
+"<h4>" + shop[i].title + "</h4>"
